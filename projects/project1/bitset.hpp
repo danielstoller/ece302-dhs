44a43d2f366ff93f40f12ccce170366aba1b8877
danielstoller <danhstoller@gmail.com>
2023-01-24 00:16:42 -0500

    All comments are added to hpp

--- projects/project1/bitset.hpp
+++ projects/project1/bitset.hpp
@@ -7,45 +7,81 @@
 class Bitset{
 public:
 
-  // TODO COMMENT
+  //** Default constructor
+  //purpose: Set bitset to size N=8, all initialized to 0
+  //@param No parameter
   Bitset();
 
-  // TODO COMMENT
+  //** Constructor
+  //purpose: Set bitset to int N=size, all initialized to 0
+  //@param: int Size for bitset
+  //@throw: If N<=0, bitset is invalid
   Bitset(intmax_t size);
 
-  // TODO COMMENT
+  //** Constructor
+  //purpose: Set bitset to size N=string length, all initialized to 0
+  //@param: int Size for bitset
+  //@throw: If string contains character other than 0 or 1, bitset is invalid
   Bitset(const std::string & value);
 
-  // TODO COMMENT
+  //** Destructor
+  //purpose: Deallocated to prevent memory leakage
+  //@post Bitset will be deallocated
   ~Bitset();
 
+  //** 
+  //=======================================
   Bitset(const Bitset & ) = delete;
   Bitset & operator=(const Bitset &) = delete;
 
-  // TODO COMMENT
+  //** Accessor Function
+  //@param None
+  //@return Size as int
+  //@throw if string, must turn into an int
   intmax_t size() const;
 
-  // TODO COMMENT
+  //** Accessor Function
+  //@param None
+  //@return Boolean isValid
   bool good() const;
 
-  // TODO COMMENT
+  //** Mutator Function
+  //purpose: Set nth bit to 1
+  //@param int Index of the bit to set
+  //@post if nth bit was 0, now it's 1
   void set(intmax_t index);
 
-  // TODO COMMENT
+  //** Mutator Function
+  //purpose: Set nth bit to 0
+  //@param int Index of the bit to set
+  //@post if nth bit was 1, now it's 0
   void reset(intmax_t index);
 
-  // TODO COMMENT
+  //** Mutator Function
+  //purpose: Toggle nth bit
+  //@param: int Index of the bit to set
+  //@post: if nth bit was 0, now it's 1
+  //@post: if nth bit was 1, now it's 0
   void toggle(intmax_t index);
 
-  // TODO COMMENT
+  //** Check Function
+  //purpose: Check if nth bit is a 1 or 0
+  //@param: Index of bit
+  //@return: Boolean to show whether the bit is 1 or 0
   bool test(intmax_t index);
 
-  // TODO COMMENT
+  //** Accessor Function
+  //purpose: turns the bitset of ints into a string of characters 0 and 1
+  //@param: None
+  //@return: bitset as a string
   std::string asString() const;
 
 private:
 
-  // TODO
+  //true if valid, false if not valid
+  unsigned int bitSize;
+  int* bitset = new int[bitSize];
+  bool isValid;
 };
 
 #endif
