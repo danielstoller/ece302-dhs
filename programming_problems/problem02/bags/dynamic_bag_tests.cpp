7733d2dacf519eba9e450d47d8ad3929fa14a2c8
danielstoller <danhstoller@gmail.com>
2023-02-04 17:05:24 -0500

    All tests passed!!

--- programming_problems/problem02/bags/dynamic_bag_tests.cpp
+++ programming_problems/problem02/bags/dynamic_bag_tests.cpp
@@ -18,3 +18,59 @@ TEST_CASE("Calling all public members", "[DynamicBag]"){
   b.contains(0);
 }
 
+TEST_CASE("Default constructor creates bag of size 0","[DynamicBag]"){
+  DynamicBag<int> b;
+ 
+  REQUIRE(b.getCurrentSize() == 0);
+}
+
+TEST_CASE("Maximum size is 100", "[DynamicBag]"){
+  DynamicBag<int> b;
+ 
+  REQUIRE(b.getCurrentSize() < 100);
+  //REQUIRE(b.maxsize == 100);
+}
+
+TEST_CASE("Item is added successfully", "[DynamicBag]"){
+  DynamicBag<int> b;
+
+  REQUIRE(b.add(1));
+  REQUIRE(b.contains(1));
+}
+
+
+TEST_CASE("Item is removed successfully", "[DynamicBag]"){
+  DynamicBag<int> b;
+
+  b.add(1);
+  b.add(2);
+
+  REQUIRE(b.remove(2));
+  REQUIRE_FALSE(b.contains(2));
+  REQUIRE(b.contains(1));  
+
+
+}
+
+TEST_CASE("All items are cleared from bag", "[DynamicBag]"){
+  DynamicBag<int> b;
+
+  b.add(1);
+  b.add(2);
+  b.clear();
+
+  REQUIRE_FALSE(b.contains(1));
+  REQUIRE_FALSE(b.contains(2));
+  REQUIRE(b.getCurrentSize() == 0);
+  REQUIRE(b.isEmpty());
+
+}
+
+TEST_CASE("Get the frequency of an item in the bag", "[DynamicBag]"){
+  DynamicBag<int> b;
+
+  b.add(1);
+  b.add(1);
+
+  REQUIRE(b.getFrequencyOf(1) == 2);
+}
